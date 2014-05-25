90997ab3639ba1f159cf9afc58bc2baa4da43094
tizimah <tizimah@gmail.com>
2014-05-26 01:20:39 +0900

    .

---
+++ src/app/model/lecture.js
@@ -0,0 +1,27 @@
+define( [ 'backbone'], function(Backbone) {
+	return Backbone.Model.extend( {
+
+		defaults: {
+	        VIDEO_SEQ: '273',
+	        LECTURE_SEQ: '6209',
+	        VIDEO_SOURCE: '<iframe width=\"640\" height=\"360\" src=\"http://www.youtube.com/embed/fcHhTI3MBbM?feature=player_embedded\" frameborder=\"0\" allowfullscreen></iframe>',
+	        VIDEO_SUBJECT_TYPE: '02',
+	        VIDEO_SUBJECT_TYPE_NM: '기회의 포착',
+	        VIDEO_TITLE: '02-서른 한살 청년기업가',
+	        VIDEO_MINUTE: '1',
+	        VIDEO_SECOND: '37',
+	        VIDEO_TEXT: '모든 아이디어의 시작은 와일드 크리에이티브이다.\r\n나는 Woker인가, Walker인가?\r\n풋내기 기업가가 말하는 창업의 가치!',
+	        RECOMMEND_CNT: '0',
+	        VIEW_CNT: '76',
+	        IMAGE_URL: 'http://www.yesleaders.com/miplatform/file_store/notice/201303/mofix74/20121228_02.jpg',
+	        LEADER_NM: '윤정용',
+	        COMPANY_NM: 'Wildcreative',
+	        UNIVERSITY_NM: '삼육대학교',
+	        VIDEO_SOURCE_URL: 'http://www.youtube.com/embed/fcHhTI3MBbM?feature=player_embedded'
+		},
+
+		initialize: function() {
+			// VIDEO_SOURCE 파싱해서 src 속성을 VIDEO_SOURCE_URL에 저장할것.
+		}
+	});
+} );
